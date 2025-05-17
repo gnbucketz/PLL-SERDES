3af23c7d963c6b596aa2feaa1a8a1773d795213f
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-05-16 17:13:43 -0700

    Update phase_detector.v

--- phase_detector.v
+++ phase_detector.v
@@ -3,26 +3,23 @@
 module PFD(
     input d1,
     input d2,
-    input clk1,
-    input clk2,
+    input clk,
     
     output reg up,
     output wire q1_n,
     output reg dn,
     output wire q2_n
-    );
-assign q1_n = ~up;
-assign q2_n = ~dn;   
+    ); 
 assign rst = up & dn;
 
-always @(posedge clk1 or posedge rst) begin
+always @(posedge clk or posedge rst) begin
     if (rst)
         up <= 0;
     else
         up <= d1;
 end
 
-always @(posedge clk2 or posedge rst) begin
+always @(posedge clk or posedge rst) begin
     if (rst)
         dn <= 0;
     else
