16c83c255ea8d77e891108619435c10367a091bd
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-05-23 23:25:17 -0700

    Update phase_detector.v

--- phase_detector.v
+++ phase_detector.v
@@ -1,29 +1,20 @@
 `timescale 1ns / 1ps
 
 module PFD(
-    input d1,
-    input d2,
-    input clk,
-    
-    output reg up,
-    output wire q1_n,
-    output reg dn,
-    output wire q2_n
-    ); 
-assign rst = up & dn;
-
-always @(posedge clk or posedge rst) begin
-    if (rst)
+ input d1, d2, clk,
+ output reg up, dn,
+ output wire rst
+ );
+ 
+ assign rst = up & dn;
+ 
+always @ (posedge clk or posedge rst) begin
+    if (rst) begin
         up <= 0;
-    else
-        up <= d1;
-end
-
-always @(posedge clk or posedge rst) begin
-    if (rst)
         dn <= 0;
-    else
+    end else begin
+        up <= d1;
         dn <= d2;
 end
-    
+end
 endmodule
