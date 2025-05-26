aa8e565559f7ab54f9cf770093fe7ab937696eca
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-05-25 21:13:53 -0700

    Update loopfilter.v

--- loopfilter.v
+++ loopfilter.v
@@ -12,7 +12,7 @@ always @ (posedge clk or posedge rst) begin
     case({up, dn})
         2'b00: speed_var <= speed_var;
         2'b01: speed_var <= (speed_var > 0) ? speed_var - 1 : 8'd0; 
-        2'b10: speed_var <= (speed_var > 0) ? speed_var + 1 : 8'd255; 
+        2'b10: speed_var <= (speed_var < 8'd255) ? speed_var + 1 : 8'd255; 
         2'b11: speed_var <= speed_var;
         default: speed_var <= 8'd128;
         endcase
