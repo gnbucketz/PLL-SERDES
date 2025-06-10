c1a1d37fff5cb12a2d09740f6c4e43895fdb1145
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-06-10 00:33:26 -0700

    Update dco.v

--- dco.v
+++ dco.v
@@ -1,22 +1,25 @@
 `timescale 1ns / 1ps
 
-module DCO(
-input [23:0] speed_var, 
-input [23:0] mod,
+module DCO#(
+parameter bit_count = 24,
+parameter min_value = 0
+)(
+input [bit_count-1:0] speed_var, 
+input [bit_count-1:0] mod,
 input clk, 
 input rst, 
 output reg signal_out
     );
 
-reg [23:0] accum = 0;
+reg [bit_count-1:0] accum;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        accum <=24'd0;
-        signal_out <=1'd0;
+        accum <= min_value;
+        signal_out <= 1'b0;
     end else begin
     if (accum >= mod) begin
-        accum <= 0;
+        accum <= min_value;
         signal_out <= ~signal_out;
     end else begin
         accum <= accum + speed_var;
