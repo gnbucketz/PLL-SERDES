3764d2127c8fbc9e7a701cb2205bf9f8f0d11cb7
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-06-10 00:23:12 -0700

    Update loopfilter.v

--- loopfilter.v
+++ loopfilter.v
@@ -1,20 +1,24 @@
 `timescale 1ns / 1ps
 
-module LoopFilter(
+module LoopFilter#(
+parameter bit_count = 24,
+parameter default_speed = 8388608,
+parameter max_speed = 16777215,
+parameter min_speed = 0
+)(
     input up, dn, rst, clk,
-    output reg [23:0] speed_var
+    output reg [bit_count-1:0] speed_var
     );
 
 always @ (posedge clk or posedge rst) begin
     if (rst) begin
-        speed_var <= 24'd8388608;
+        speed_var <= default_speed;
     end else begin 
     case({up, dn})
         2'b00: speed_var <= speed_var;
-        2'b01: speed_var <= (speed_var > 0) ? speed_var - 1 : 24'd0; 
-        2'b10: speed_var <= (speed_var < 24'd16777215) ? speed_var + 1 : 24'd16777215; 
+        2'b01: speed_var <= (speed_var > 0) ? speed_var - 1 : min_speed; 
+        2'b10: speed_var <= (speed_var < max_speed) ? speed_var + 1 : max_speed; 
         2'b11: speed_var <= speed_var;
-        default: speed_var <= 24'd8388608;
         endcase
         end
     end
