412c8b9d4cdc20ccd7a6c63b0b788bbca1851c97
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-06-11 00:28:42 -0700

    Create frequency_divider.v

---
+++ frequency_divider.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module Frequency_Divider#(
+parameter n = 1
+//1 d FF = divide 2, 2 d FF = divide 4, 3 d FF = divide by 8
+)(
+input clk, input rst, input d1, input d2, input d3, input qn1, input qn2, input qn3, output reg q1, output reg q2, output reg q3
+
+   
+   
+    );
+endmodule
