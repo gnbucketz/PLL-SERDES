8a8dce72b1c13e75c63d08a260db56b2e08b8be2
gnbucketz <143123145+gnbucketz@users.noreply.github.com>
2025-09-30 11:27:01 -0700

    Update dco.v

--- dco.v
+++ dco.v
@@ -1,29 +1,24 @@
 `timescale 1ns/1ps
-module DCO#(
-  parameter bit_count = 24
+module DCO #(
+  parameter bit_count = 24          
 )(
-  input  wire [bit_count-1:0] speed_var,
-  input  wire [bit_count-1:0] mod,
-  input  wire clk,
-  input  wire rst,
-  output reg  signal_out,
-  output reg  [bit_count-1:0] accum
+  input  wire                 sys_clk,    
+  input  wire                 ext_rst,    
+  input  wire [bit_count-1:0] speed_var,  
+  input  wire [bit_count-1:0] mod,        
+  output reg                  signal_out, 
+  output reg  [bit_count-1:0] accum       
 );
-  reg [bit_count:0] sum; // one bit wider
-
-  always @(posedge clk or posedge rst) begin
+    
+  wire rst = ext_rst;  
+  always @(posedge sys_clk or posedge ext_rst) begin
     if (rst) begin
       accum      <= {bit_count{1'b0}};
       signal_out <= 1'b0;
     end else begin
-      sum = {1'b0,accum} + {1'b0,speed_var};  // wide add
-      if (sum >= {1'b0,mod}) begin
-        accum      <= sum - {1'b0,mod};       // modulo subtract
-        signal_out <= ~signal_out;            // toggle on wrap
-      end else begin
-        accum      <= sum[bit_count-1:0];
-        // signal_out unchanged
-      end
+      accum      <= accum + speed_var + mod;  
+      signal_out <= accum[bit_count-1];       
     end
   end
+
 endmodule
